f9b25809aa1fd20338303bb2672c9734325b9bca
fabio <fabio.s.a.proweb@gmail.com>
2018-12-16 01:06:14 -0200

    remove files

--- api/put.php
+++
@@ -1,5 +0,0 @@
-<?php 
-
-$array('nome'=>'Pessoal');
-
-?>
--- application/config/config.db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-	class Config{
-		public $host = 'localhost';
-		public $base = 'devel_my_system';
-		public $user = 'root';
-		public $pass = '';
-	}
-?>
